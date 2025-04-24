cbb7efe33bb27b44a457394ff842fb4b61c9fbc0
João Vítor Mamede <jvalencarmamede@gmail.com>
2025-04-24 12:32:48 -0300

    Usuario ADM pré-cadastrado no pacote Config, após rodar a primeira vez com isso, deve-se comentar a classe!

--- pilot/src/main/java/com/zeiss/pilot/config/AdminInitializer.java
+++ pilot/src/main/java/com/zeiss/pilot/config/AdminInitializer.java
@@ -1,33 +1,33 @@
-// package com.zeiss.pilot.config;
+package com.zeiss.pilot.config;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-// import com.zeiss.pilot.entity.Usuario;
-// import com.zeiss.pilot.repository.UsuarioRepository;
+import com.zeiss.pilot.entity.Usuario;
+import com.zeiss.pilot.repository.UsuarioRepository;
 
-// import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
-// @Configuration
-// public class AdminInitializer {
+@Configuration
+public class AdminInitializer {
 
-//     @Autowired
-//     private UsuarioRepository usuarioRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
-//     @Autowired
-//     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-//     @PostConstruct
-//     public void initAdmin() {
-//         if (usuarioRepository.findByEmail("admin@zeiss.com").isEmpty()) {
-//             Usuario admin = new Usuario();
-//             admin.setNome("Administrador");
-//             admin.setEmail("admin@zeiss.com");
-//             admin.setRole("ADMIN");
-//             admin.setSenha(passwordEncoder.encode("senhaSegura123"));
-//             usuarioRepository.save(admin);
-//             System.out.println("🛡️ Usuário ADMIN criado com sucesso!");
-//         }
-//     }
-// }
+    @PostConstruct
+    public void initAdmin() {
+        if (usuarioRepository.findByEmail("admin@zeiss.com").isEmpty()) {
+            Usuario admin = new Usuario();
+            admin.setNome("Administrador");
+            admin.setEmail("admin@zeiss.com");
+            admin.setRole("ADMIN");
+            admin.setSenha(passwordEncoder.encode("teste123"));
+            usuarioRepository.save(admin);
+            System.out.println("🛡️ Usuário ADMIN criado com sucesso!");
+        }
+    }
+}
